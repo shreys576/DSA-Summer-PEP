54e85aadf9bedc4316e2e0efa732c50bcf5016e6
Shrey Shukla <shreys576@gmail.com>
2026-07-16 10:01:07 +0530

    Practiced Validating a BST

--- Self_Practice/Binary_Trees/Class.cpp
+++ Self_Practice/Binary_Trees/Class.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 
 class node{
@@ -14,6 +15,46 @@ class node{
     }
 };
 
+
+node* insert(node* root, int key){
+    node* newnode = new node(key);
+    if(root == NULL){
+        root = newnode;
+        return root;
+    }
+    if(root -> val < key){
+        root -> right = insert(root -> right, key);
+    }
+    else{
+        root -> left = insert(root -> left, key);
+    }
+    return root;
+}
+
+bool helper(node* root, long long low, long long high){
+    if(root == NULL) return true;
+    if(root -> val < low || root -> val > high){
+        return false;
+    }
+    return helper(root -> left, low, root -> val) && helper(root -> right, root -> val, high);
+}
+
+bool ValidBST(node* root){
+    return helper(root, LLONG_MIN, LLONG_MAX);
+
+}
+
 int main(){
+    node* root = NULL;
+    root = insert(root, 8);
+    root = insert(root, 4);
+    root = insert(root, 10);
+    root = insert(root, 2);
+    root = insert(root, 6);
+    root = insert(root, 20);
+    root -> right -> left = new node(6); 
+
+    bool check = ValidBST(root);
+    cout << check << endl;
     
 }
